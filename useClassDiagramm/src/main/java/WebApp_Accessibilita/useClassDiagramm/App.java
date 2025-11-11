992ec52764ac16c2b749dda7534f7e588b7dcc3c
allypm <a.perezmurillo@studenti.unibg.it>
2025-11-11 17:57:51 +0100

    use case base
    creazione package maven e use case generale con papyrus

---
+++ useClassDiagramm/src/main/java/WebApp_Accessibilita/useClassDiagramm/App.java
@@ -0,0 +1,10 @@
+package WebApp_Accessibilita.useClassDiagramm;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
