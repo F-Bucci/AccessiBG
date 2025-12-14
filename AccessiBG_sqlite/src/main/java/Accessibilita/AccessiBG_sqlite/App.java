d41af821da0b13d9254a9813e9a1ef44c8b06b8b
F-Bucci <f.bucci1@studenti.unibg.it>
2025-12-14 19:13:07 +0100

    First draft Data Layer

---
+++ AccessiBG_sqlite/src/main/java/Accessibilita/AccessiBG_sqlite/App.java
@@ -0,0 +1,13 @@
+package Accessibilita.AccessiBG_sqlite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ AccessiBG_sqlite/src/main/java/Accessibilita/AccessiBG_sqlite/CreateDB.java
@@ -0,0 +1,24 @@
+package Accessibilita.AccessiBG_sqlite;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class CreateDB {
+	// questo � il percordo relativo del file che contiene il DB
+		public static String DB_REL_FILE = "db/indirizzi.db3";
+		public static String DB_URL = "jdbc:sqlite:" + DB_REL_FILE;
+
+		public static void main(String[] args) throws IOException, SQLException {
+			Connection conn = DriverManager.getConnection(DB_URL);
+			DatabaseMetaData meta = conn.getMetaData();
+			System.out.println("The driver name is " + meta.getDriverName());
+			System.out.println("A new database has been created.");
+			// controllo che il file esista a questo punto
+			System.out.println("il file esiste? " + new File(DB_REL_FILE).exists());
+}
+
+}
