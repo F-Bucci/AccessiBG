6c683c3d3c6188e7392ac13bdd193f42b19cd0de
CuniLuizo <l.cuni3@studenti.unibg.it>
2025-12-26 18:52:06 +0100

    Aggiornamento MainPage.java
    Ho effettuato un aggiornamento del fail MainPage.java

--- accessibilita-unibg/src/main/java/it/unibg/accessibilita/base/ui/MainPage.java
+++ accessibilita-unibg/src/main/java/it/unibg/accessibilita/base/ui/MainPage.java
@@ -18,6 +18,16 @@ import it.unibg.accessibilita.base.ui.component.ViewToolbar;
 @Menu(order = 0, icon = "vaadin:clipboard-check", title = "Main Page")
 public class MainPage extends VerticalLayout { 
 
+	private Card createCard(String testo) {
+		Card card = new Card();
+		
+		Icon iconTemp = LumoIcon.PHOTO.create();
+		
+		card.setMedia(iconTemp);
+		card.add(testo);
+		
+		return card;
+	};
 
     public MainPage() {
     	
@@ -27,12 +37,32 @@ public class MainPage extends VerticalLayout {
     	
         add(new H3("Ciao, Vaadin!")); 
 
-        Card iconCard = new Card();
-        Icon icon = LumoIcon.PHOTO.create();
-        iconCard.setMedia(icon);
-        iconCard.add(
-                "Sede di Dalmine");
-        add(iconCard);
+        
+        /* CREAZIONE CARD */
+        /*
+         * Attualmente le card vengono create in questa maniera,
+         * ma appena riusciremo ad effettuare la comunicazione con
+         * il DB modificheremo le righe di codice sottostanti per
+         * renderli più efficienti e più adatti al contesto.
+         */
+        
+        Card iconCard1 = createCard("Sede di Dalmine");
+        Card iconCard2 = createCard("Sede di Kilometro Rosso");
+        Card iconCard3 = createCard("Sede di Caniana");
+        Card iconCard4 = createCard("Sede Palazzo Bassi-Rathgeb");
+        Card iconCard5 = createCard("Sede di Pignolo");
+        Card iconCard6 = createCard("Sede di Sant'Agostino");
+        Card iconCard7 = createCard("Sede di Rosate");
+        Card iconCard8 = createCard("Sede di Salvecchio");
+        
+        add(iconCard1);
+        add(iconCard2);
+        add(iconCard3);
+        add(iconCard4);
+        add(iconCard5);
+        add(iconCard6);
+        add(iconCard7);
+        add(iconCard8);
 
     }
 }
