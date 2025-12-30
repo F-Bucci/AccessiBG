c23e7cd8682ae99d1d6c25aa3d9568068c237fd2
DanUniBG <d.monteleone2@studenti.unibg.it>
2025-12-30 21:57:57 +0100

    Refactor SediView to use AppTabs and modularize content
    Replaces TabSheet with the custom AppTabs component and moves tab content into separate methods for better modularity and readability. Updates the layout and content structure for the 'Generale', 'Edifici', and 'Mappa' tabs, and improves code organization.

--- accessibilita-unibg/src/main/java/it/unibg/accessibilita/base/ui/SediView.java
+++ accessibilita-unibg/src/main/java/it/unibg/accessibilita/base/ui/SediView.java
@@ -1,5 +1,6 @@
 package it.unibg.accessibilita.base.ui;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Hr;
@@ -14,6 +15,7 @@ import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 
 import it.unibg.accessibilita.base.ui.MainLayout;
 import it.unibg.accessibilita.base.ui.component.AppFooter;
+import it.unibg.accessibilita.base.ui.component.AppTabs;
 import it.unibg.accessibilita.base.ui.component.BoxInfo;
 import it.unibg.accessibilita.base.ui.component.GoogleMapCard;
 import it.unibg.accessibilita.base.ui.component.ImageCard;
@@ -35,77 +37,80 @@ public class SediView extends VerticalLayout{
 		
 		Hr divisoreTitolo = new Hr();
 		
-		TabSheet tabSheet = new TabSheet();
-		tabSheet.setWidthFull();
+		AppTabs tabs = new AppTabs();
 		
-		VerticalLayout tabGenerale = new VerticalLayout();
-		tabGenerale.setPadding(false);
-		tabGenerale.setSpacing(true);
-		tabGenerale.setWidthFull();
-		tabGenerale.setMaxWidth("1200px");
-		tabGenerale.addClassNames(LumoUtility.Margin.Horizontal.AUTO);
+		tabs.addTab("Generale",creatGeneraleContent());
+		tabs.addTab("Edifici", createEdificioContent());
+		tabs.addTab("Mappa", createMappaContent());
+		AppFooter footer = new AppFooter();
 		
-		HorizontalLayout bodySedi = new HorizontalLayout();
-		bodySedi.setWidthFull();
-		bodySedi.setSpacing(true);
-
+		add(titolo, divisoreTitolo, tabs, footer);
+	}
+	
+	private Component creatGeneraleContent() {
+		VerticalLayout layout = new VerticalLayout();
+		layout.setPadding(false);
+		layout.setSpacing(true);
+		layout.setWidthFull();
+		layout.setMaxWidth("1200px");
+		layout.addClassNames(LumoUtility.Margin.Horizontal.AUTO);
 		
-		ImageCard card = new ImageCard("Sede di Dalmine", "Via via Pasubio, 3", null);//modificare dopo aver completato database
+		H3 titoloSezione = new H3("Sede");
+		titoloSezione.addClassNames(LumoUtility.Margin.Bottom.SMALL);
+		
+		HorizontalLayout rowCentrale = new HorizontalLayout();
+		rowCentrale.setWidthFull();
+		rowCentrale.setSpacing(true);
+		rowCentrale.setAlignItems(Alignment.START);
+		
+		ImageCard card = new ImageCard("Sede di Dalmine", "Via Pasubio, 3", null);
+		card.setWidth("65%");
 		card.setMinWidth("320px");
 		card.setHeight("500px");
-		card.setWidth("65%");
 		
-		//BOX SUPERIORE TAB GENERALE
-		BoxInfo infoSedeDalmine = new BoxInfo("orario", "mail e telefono", "aperto"); //modificare dopo aver completato database
-		infoSedeDalmine.setWidth("35%");
-		infoSedeDalmine.setMinWidth("250px");
-		infoSedeDalmine.setHeight("500px");
+		BoxInfo infoSede = new BoxInfo("Orario", "Contatti", "Aperto");
+		infoSede.setWidth("30%");
+		infoSede.setMinWidth("250px");
+		infoSede.setHeight("500px");
 		
-		bodySedi.add(card, infoSedeDalmine);
+		rowCentrale.add(card, infoSede);
 		
-		//BOX PARTE INFERIORE DELLA TAB GENERALE
-		BoxInfo boxSotto = new BoxInfo("Sulla Sede di Dalmine", "aggiungere informazioni aggiuntive", "altro info");
-		boxSotto.setWidthFull();
-		boxSotto.addClassName(LumoUtility.Margin.Top.XLARGE);
+		BoxInfo descBox = new BoxInfo("Sulla sede di Dalmine", "", "");
+		descBox.setWidthFull();
+		descBox.addClassNames(LumoUtility.Margin.Top.LARGE);
 		
-		tabGenerale.add(bodySedi, boxSotto);
+		layout.add(titoloSezione, rowCentrale, descBox);
 		
-		tabSheet.add("Generale", tabGenerale);
+		return layout;
+	}
 
+	private Component createEdificioContent() {
+		VerticalLayout layout = new VerticalLayout();
+		layout.setWidthFull();
+		layout.setMaxWidth("1200px");
+		layout.addClassName(LumoUtility.Margin.Horizontal.AUTO);
+		layout.setPadding(true);
 		
-		//TAB EDIFICI
-		VerticalLayout edificioTab = new VerticalLayout();
-		edificioTab.setWidthFull();
-		edificioTab.setMaxWidth("1200px");
-		edificioTab.addClassName(LumoUtility.Margin.Horizontal.AUTO);
-		edificioTab.add(new Paragraph("prova"));
-		tabSheet.add("Edifici", edificioTab);
+		layout.add(new Paragraph("In manutenzione"));
+	
+		return layout;
 		
-		//TAB MAPPA
+	}
+	
+	private Component createMappaContent(){
+		VerticalLayout layout = new VerticalLayout();
+		layout.setWidthFull();
+		layout.setMaxWidth("1200px");
+		layout.addClassName(LumoUtility.Margin.Horizontal.AUTO);
 		
-		VerticalLayout mappaTab = new VerticalLayout();
-		mappaTab.setWidthFull();
-		mappaTab.setMaxWidth("1200px");
-		mappaTab.addClassName(LumoUtility.Margin.Horizontal.AUTO);
-		/*modificare in seguito, l'url si ottiene andando su Google Maps
-		 * cliccare sulla posizione che  si vuole ottenere
-		 * infine cliccare su "incorpora mappa"
-		 * copiare la parte tra virgolette dopo src.
-		 */
-		mappaTab.setPadding(true);
-		H3 titoloMappa = new H3("MAPPA DELLA SEDE DI "); // aggiungere dopo implementazione del database
-		titoloMappa.addClassNames(LumoUtility.Margin.Bottom.SMALL);
+		H3 titoloMappa = new H3("MAPPA DELLA SEDE");
+		titoloMappa.addClassNames(LumoUtility.Margin.Horizontal.AUTO);
+		
 		String urlDalmine = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1172.714378374875!2d9.598500453105066!3d45.64679386157501!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sit!2sit!4v1767047582946!5m2!1sit!2sit";
 		GoogleMapCard mappaSede = new GoogleMapCard(urlDalmine);
 		
-		mappaTab.add(titoloMappa, mappaSede);
-		tabSheet.add("Mappa", mappaTab);
-		
-		expand(tabSheet);
-		
-		//FOOTER
-		AppFooter footer = new AppFooter();
-		
-		add(titolo, divisoreTitolo, tabSheet, footer);
+		layout.add(titoloMappa, mappaSede);
+		return layout;
+
 	}
 }
