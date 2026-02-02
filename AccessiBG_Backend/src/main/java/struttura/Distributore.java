911b810b0bfd16c7be0921f3ec8074c0ca4c93b7
CuniLuizo <l.cuni3@studenti.unibg.it>
2026-02-02 16:35:29 +0100

    Update Distributore.java
    Co-Authored-By: DanUniBG <126773572+DanUniBG@users.noreply.github.com>

--- AccessiBG_Backend/src/main/java/struttura/Distributore.java
+++ AccessiBG_Backend/src/main/java/struttura/Distributore.java
@@ -1,30 +1,47 @@
 package struttura;
 
-public class Distributore {
+public class Distributore implements ElementoMappa{
 	private int id;
+	private String nome;
 	private TipoDistributore tipo;
 	private boolean accessibile;
-	private String posizione;
+	private double x;
+	private double y;
+	private String pathFoto;
+	private String pathPercorso;
 	private int piano;
 	
-	public Distributore(int id, TipoDistributore tipo, String posizione, boolean accessibile, int piano) {
+	public Distributore(int id, String nome,TipoDistributore tipo, boolean accessibile, double x, double y, String pathFoto,String pathPercorso, int piano) {
 		this.id=id;
+		this.nome = nome;
 		this.tipo = tipo;
-		this.posizione = posizione;
 		this.accessibile = accessibile;
+		this.x=x;
+		this.y=y;
 		this.piano=piano;
+		this.pathFoto=pathFoto;
+		this.pathPercorso=pathPercorso;
 	}
-	
 	public TipoDistributore getTipo() { return tipo; }
-    // public void setTipo(TipoDistributore tipo) {this.tipo = tipo; }
-
-    public String getPosizione() { return posizione; }
-    // public void setPosizione(String posizione) { this.posizione = posizione; }
+//    public void setTipo(TipoDistributore tipo) {this.tipo = tipo; }
 
     public boolean getAccessibile() { return accessibile; }
-    // public void setAccessibile(boolean accessibile) { this.accessibile = accessibile; }
+//    public void setAccessibile(boolean accessibile) { this.accessibile = accessibile; }
     
 	public int getPiano() {return piano;}
 	
 	public int getId() {return id;}
+	
+	public double getX() {return x;}
+	public double getY() {return y;}
+	@Override
+	public String getPathPercorso() {
+		return pathPercorso;
+	}
+	public String getPathFoto() {
+		return pathFoto;
+	}
+	public String getNome() {
+		return nome;
+	}
 }
