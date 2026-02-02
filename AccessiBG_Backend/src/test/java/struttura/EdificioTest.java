a56272cfa1b059b93badf6b3b2668fa884e63b17
CuniLuizo <l.cuni3@studenti.unibg.it>
2026-02-02 23:51:19 +0100

    Commit d'emergenza
    recuperato vecchio stato del progetto per evitare problemi che sono sorti durante i tentativi di spostamento dei progetti

--- AccessiBG_Backend/src/test/java/struttura/EdificioTest.java
+++ AccessiBG_Backend/src/test/java/struttura/EdificioTest.java
@@ -1,12 +1,10 @@
 package struttura;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import java.util.List;
 
 import org.junit.Test;
+
+
 
 public class EdificioTest {
 
