404981587b7741c92fa93a33716b9963ed77758c
F-Bucci <f.bucci1@studenti.unibg.it>
2026-02-04 16:31:44 +0100

    Update AppTest.java

--- AccessiBG_sqlite/src/test/java/Accessibilita/AccessiBG_sqlite/AppTest.java
+++ AccessiBG_sqlite/src/test/java/Accessibilita/AccessiBG_sqlite/AppTest.java
@@ -1,38 +1,37 @@
 package Accessibilita.AccessiBG_sqlite;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+/**    public AppTest( String testName )
+*  {
+*       super( testName );
     }
 
     /**
      * @return the suite of tests being tested
-     */
-    public static Test suite()
+    
+ //   public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
 
     /**
      * Rigourous Test :-)
-     */
+     
     public void testApp()
     {
         assertTrue( true );
     }
+    */
 }
